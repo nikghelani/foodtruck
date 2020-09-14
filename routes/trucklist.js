ef2fbfb3890c17b5545602f33cb9cc81b3abd05f
Nikhilkumar, Ghelani <ghelani.nik@gmail.com>
2020-09-13 23:17:08 -0600

    added Trucklist page with basic layout and pagination

--- routes/trucklist.js
+++ routes/trucklist.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-  res.render('trucklist', { title: 'trucks in towns' });
+  res.render('trucklist', { title: 'Trucks in Town' });
 });
 
 module.exports = router;  
