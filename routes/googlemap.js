65b4f2dee95b84648ff3b853bca16c76725cf73d
nikghelani <ghelani.nik@gmail.com>
2020-09-14 18:41:42 -0600

    removed google api

--- routes/googlemap.js
+++ routes/googlemap.js
@@ -6,19 +6,8 @@ router.get('/location', function(req, res) {
   res.render('googlemap', { title: 'Map of your city' });
 });
 
-var map;
-
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8
-  });
-}
-
-var gmap = initMap;
-
 
 module.exports = router;
-module.exports = gmap; 
+
 
 
