80b5cfa0b461df152eecc7abd1125d4dc7cee68a
nikghelani <ghelani.nik@gmail.com>
2020-09-19 16:11:15 -0600

    controller file for inserting trucks in database

---
+++ controllers/registertruck.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var Truck = require('../models/trucks');
+
+exports.registerForm = function (req, res){
+    res.render('/registertruck', { Truck : {}, errors: [] });
+}
+
+exports.create = function(req, res){
+  var title = req.body.title;
+  var description = req.body.description;
+  var category = req.body.category;
+  var bizlocation = req.body.bizlocation;
+  var streetlocation = req.body.streetlocation;
+  var direction = req.body.direction;
+
+  var newTruck = new Truck({
+    title : title,
+    description : description,
+    category : category,
+    bizlocation : bizlocation,
+    streetlocation : streetlocation,
+    direction : direction
+  });
+
+  newTruck.save(function(err){
+    if (err) {
+      res.render('/registertruck', { Truck: newTruck, errors: err.errors });
+    } else {
+      res.redirect('/');
+      console.log('Your Truck has been registerd Succsessfully');
+    }
+  });
+};
--- routes/registertruck.js
+++ routes/registertruck.js
@@ -1,25 +1,10 @@
 var express = require('express');
-const app = require('../app');
-var Truck = require('../models/trucks');
+var registerController = require('../controllers/registertruck');
 var router = express.Router();
 
-router.get('/', function(req, res) {
-  res.render('registertruck', { title: 'Register your Truck Here!!!!' });
-});
+router
+  .route('/registertruck')
+  .get(registerController.registerForm)
+  .post(registerController.create)
 
-router.post('/', function (req, res, next){
-  Truck.create({
-    title : req.body.title,
-    description : req.body.description,
-    category : req.body.category,
-    bizlocation : req.body.bizlocation,
-    streetlocation : req.body.streetlocation,
-    direction : req.body.direction
-  }, function (err, result){
-    if (err) console.log(err)
-  });
-  // Truck.create(registerinfo, function(err, result){
-  //   console.log('inserted data');
-  // });
-});
 module.exports = router;
