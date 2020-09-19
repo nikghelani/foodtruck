6081d9df319a7f1452073fb6ddfc88a7c42951ee
nikghelani <ghelani.nik@gmail.com>
2020-09-19 11:31:14 -0600

    push function for register foodtruck

--- routes/registertruck.js
+++ routes/registertruck.js
@@ -7,4 +7,19 @@ router.get('/', function(req, res) {
   res.render('registertruck', { title: 'Register your Truck Here!!!!' });
 });
 
+router.post('/', function (req, res, next){
+  Truck.create({
+    title : req.body.title,
+    description : req.body.description,
+    category : req.body.category,
+    bizlocation : req.body.bizlocation,
+    streetlocation : req.body.streetlocation,
+    direction : req.body.direction
+  }, function (err, result){
+    if (err) console.log(err)
+  });
+  // Truck.create(registerinfo, function(err, result){
+  //   console.log('inserted data');
+  // });
+});
 module.exports = router;
